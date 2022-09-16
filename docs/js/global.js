5f309775e0d6555993d5ffa43180cac1422092de
Siddesh <siddesh@ajency.in>
2022-09-16 20:56:49 +0530

    Navigation mobile ui and functionality

--- docs/js/global.js
+++ docs/js/global.js
@@ -1,16 +1,31 @@
 $(function(){
     console.log( "ready!" );
     $('.language-selector').click(function(e){
-        $(".language-selector").toggleClass('active')
-        $('.language-list').toggleClass('active')
+        $(".language-selector, .language-list").toggleClass('active')
+        if($('.hamburger').hasClass('opened')){
+            $('.hamburger, .mobile-nav-list').removeClass('opened')
+        }
+        else{
+            $('.overlay').toggleClass('opened')
+        }
     })
     $('.langauge-item').click(function(){
         $(this).siblings().removeClass('active');
         $(this).addClass('active');
         $(".language-selector span").text($(this).text())
         setTimeout(function(){
-            $(".language-selector").removeClass('active')
-            $('.language-list').removeClass('active')
+            $(".language-selector, .language-list").removeClass('active')
+            $('.overlay').removeClass('opened')
         },100)
+    })
+    $('.hamburger').click(function(){
+        $(this).toggleClass('opened');
+        if($('.language-selector').hasClass('active')){
+            $(".language-selector, .language-list").removeClass('active')
+        }
+        else{
+            $('.overlay').toggleClass('opened')
+        }
+        $('.mobile-nav-list').toggleClass('opened');
     })
 })
