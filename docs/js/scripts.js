e5aa9ca9fbea14093c6458eb580c4d15bf287a94
Mamata <mamata@ajency.in>
2022-09-21 13:15:00 +0530

    overview page - video player CTA & Popup

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -5,3 +5,19 @@ $(".faq-collapse").on("show.bs.collapse", function () {
 $(".faq-collapse").on("hide.bs.collapse", function () {
   console.log($(this).prev().removeClass("show"));
 });
+
+// video player
+var player = videojs("download-now-video", {
+  fluid: true
+});
+
+$("#video-CTA-modal").on("show.bs.modal", function (e) {
+  player.play("muted");
+  player.muted(false);
+});
+
+$("#video-CTA-modal").on("hide.bs.modal", function (e) {
+  player.pause();
+  player.play("pause");
+  player.muted(true);
+});
